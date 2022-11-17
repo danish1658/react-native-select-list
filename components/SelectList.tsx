3d84753b1abe033954f96ddc742f01de8ca2abff
Justin Reed <jreedatl21@gmail.com>
2022-11-17 06:35:38 -0500

    fixed formatting

--- components/SelectList.tsx
+++ components/SelectList.tsx
@@ -228,29 +228,29 @@ const SelectList: React.FC<SelectListProps> =  ({
                                     })
                                     :
                                     (allowNewEntries)
-                                        ?
-                                        <TouchableOpacity style={[styles.option, dropdownItemStyles]} onPress={() => {
-                                             setSelected(enteredVal)
-                                            let key = data.length+1
-                                            data.push({key:key,value:enteredVal})
-                                            setSelected(key);
-                                            setSelectedVal(enteredVal);
-                                            slideup()
-                                            setTimeout(() => setFilteredData(data), 800)
+                                    ?
+                                    <TouchableOpacity style={[styles.option, dropdownItemStyles]} onPress={() => {
+                                            setSelected(enteredVal)
+                                        let key = data.length+1
+                                        data.push({key:key,value:enteredVal})
+                                        setSelected(key);
+                                        setSelectedVal(enteredVal);
+                                        slideup()
+                                        setTimeout(() => setFilteredData(data), 800)
 
-                                        }}>
-                                            <Text style={[{ fontFamily }, dropdownTextStyles]}>{enteredVal}</Text>
-                                        </TouchableOpacity>
-                                        :
-                                        <TouchableOpacity style={[styles.option, dropdownItemStyles]} onPress={() => {
-                                            setSelected(undefined)
-                                            setSelectedVal("")
-                                            slideup()
-                                            setTimeout(() => setFilteredData(data), 800)
+                                    }}>
+                                        <Text style={[{ fontFamily }, dropdownTextStyles]}>{enteredVal}</Text>
+                                    </TouchableOpacity>
+                                    :
+                                    <TouchableOpacity style={[styles.option, dropdownItemStyles]} onPress={() => {
+                                        setSelected(undefined)
+                                        setSelectedVal("")
+                                        slideup()
+                                        setTimeout(() => setFilteredData(data), 800)
 
-                                        }}>
-                                    <Text style={[{fontFamily},dropdownTextStyles]}>{notFoundText}</Text>
-                                </TouchableOpacity>
+                                    }}>
+                                        <Text style={[{fontFamily},dropdownTextStyles]}>{notFoundText}</Text>
+                                    </TouchableOpacity>
                             }
                             
                             
