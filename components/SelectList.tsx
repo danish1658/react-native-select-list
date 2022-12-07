43057972d586d35fb5e770bd7672579af136ddc0
Adam Gates <@adamg>
2022-12-08 10:05:57 +1100

    dont call setSelected on the initial mounting of component

--- components/SelectList.tsx
+++ components/SelectList.tsx
@@ -91,16 +91,12 @@ const SelectList: React.FC<SelectListProps> =  ({
         if(!_firstRender && defaultOption && oldOption.current != defaultOption.key ){
             // oldOption.current != null
             oldOption.current = defaultOption.key
-            setSelected(defaultOption.key);
             setSelectedVal(defaultOption.value);
         }
         if(defaultOption && _firstRender && defaultOption.key != undefined){
-            
             oldOption.current = defaultOption.key
-            setSelected(defaultOption.key);
             setSelectedVal(defaultOption.value);
         }
-        
     },[defaultOption])
 
     React.useEffect(() => {
