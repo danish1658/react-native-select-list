74c2691e9a32362697049d1478220b692f489907
onuraskinn <o.askin@simplydeliver.com>
2023-04-26 15:13:30 +0300

    defaultOption Support for multiple selection

--- components/MultipleSelectList.tsx
+++ components/MultipleSelectList.tsx
@@ -42,13 +42,14 @@ const MultipleSelectList: React.FC<MultipleSelectListProps> = ({
         badgeTextStyles,
         checkBoxStyles,
         save = 'key',
-        dropdownShown = false
+        dropdownShown = false,
+        defaultSelected = [], 
     }) => {
 
     const oldOption = React.useRef(null)
     const [_firstRender,_setFirstRender] = React.useState<boolean>(true);
     const [dropdown, setDropdown] = React.useState<boolean>(dropdownShown);
-    const [selectedval, setSelectedVal] = React.useState<any>([]);
+    const [selectedval, setSelectedVal] = React.useState<any>(defaultSelected);
     const [height,setHeight] = React.useState<number>(350)
     const animatedvalue = React.useRef(new Animated.Value(0)).current;
     const [filtereddata,setFilteredData] = React.useState(data);
@@ -105,7 +106,10 @@ const MultipleSelectList: React.FC<MultipleSelectListProps> = ({
         
     },[dropdownShown])
 
-
+    React.useEffect(() => {
+        setSelected(defaultSelected);
+        }, []);
+         
 
 
 
